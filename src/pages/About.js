51f1b0966a20999161b4c3a960ba4328f78a8df6
pmhansen8 <pmhansen8@gmail.com>
2024-09-09 10:27:10 -0400

    adding text

--- src/pages/About.js
+++ src/pages/About.js
@@ -4,7 +4,7 @@ export default function About(){
     <>
         <Navbar/>
         <div style={{backgroundColor: 'black', color: 'white', minHeight: '100vh'}}>
-
+        <p style={{textAlign: 'center', fontSize: '3em'}}>About</p>
         </div>
     </>
     )
